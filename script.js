15030b694bf81fa013c1ed2a7e65eb91b05ed487
Hanzala Khan <pythondeveloper360@gmail.com>
2023-11-22 06:46:12 +0500

    asds

---
+++ script.js
@@ -0,0 +1,9 @@
+function loadData(){
+    fetch('data.json')
+    .then((res)=>res.json())
+    .then((data)=>{
+        console.log(data)
+            for ()
+            document.getElementById('main')
+    })
+}
